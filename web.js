656ae19fe01590269ebe6a7a5fa45968abddf182
Sebastien PUECH <sebastien.puech@gmail.com>
2013-07-31 00:53:12 +0200

    first commit

---
+++ web.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var fs = require('fs')
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    index = fs.readFileSync('views/index.html');
+    buffer = new Buffer(index,'utf-8');
+    response.send(buffer.toString("utf8",0,buffer.length));
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
